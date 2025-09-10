1fb73009568b367db2d64492d8c8aff3e493fda9
Admin <Admin@DESKTOP-8URQBFL>
2025-09-10 15:39:23 +0500

    Rectangle and Square DONE

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -1,84 +1,222 @@
 #include <iostream>
+#include <Windows.h>
+
 using namespace std;
 
-enum Color
+namespace Geometry
 {
-	Red = 0x000000FF,
-	Green =  0x0000FF00,
-	Blue = 0x00FF0000,
-	Yellow = 0x0000FFFF,
-	Orange = 0x0000A5FF,
-	White = 0x00FFFFFF
-};
+	enum Color
+	{
+		Red = 0x000000FF,
+		Green = 0x0000FF00,
+		Blue = 0x00FF0000,
+		Yellow = 0x0000FFFF,
+		Orange = 0x0000A5FF,
+		White = 0x00FFFFFF
+	};
 
-class Shape
-{
-	Color color;
-public:
-	Shape(Color color) : color(color) {}
-	virtual double get_area()const = 0;
-	virtual double get_perimetr()const = 0;
-	virtual void draw()const = 0;
-	virtual void info()const
-	{
-		cout << "Площадь фигуры: " << get_area() << endl;
-		cout << "Периметр фигуры: " << get_perimetr() << endl;
-		draw();
-	}
-};
+#define SHAPE_TAKE_PARAMETRS   int start_x, int start_y, int line_width, Color color
+#define SHAPE_GIVE_PARAMETRS   start_x, start_y, line_width, color
 
-class Square :public Shape
-{
-	double side;
-public:
-	Square(double side, Color color) :Shape(color)
+	class Shape
 	{
-		set_side(side);
-	}
-	double get_side()const
-	{
-		return side;
-	}
-	void set_side(double side)
-	{
-		this->side = side; 
-	}
-	double get_area()const override
-	{
-		return side * side;
-	}
-	double get_perimetr()const override
-	{
-		return 4 * side;
-	}
-	void draw()const override
-	{
-		for (int i = 0; i < side; i++)
+	protected:
+		Color color;
+		int start_x;
+		int start_y;
+		int line_width;
+	public:
+		static const int MIN_START_X = 100;
+		static const int MIN_START_Y = 100;
+		static const int MAX_START_X = 1000;
+		static const int MAX_START_Y = 600;
+		static const int MIN_LINE_WIDTH = 1;
+		static const int MAX_LINE_WIDTH = 16;
+		static const int MIN_SIZE = 32;
+		static const int MAX_SIZE = 768;
+
+		Shape(SHAPE_TAKE_PARAMETRS) : color(color)
 		{
-			for (int j = 0; j < side; j++)
+			set_start_x(start_x);
+			set_start_y(start_y);
+			set_line_width(line_width);
+
+		}
+		void set_start_x(int start_x)
+		{
+			this->start_x =
+				start_x < MIN_START_X ? MIN_START_X :
+				start_x > MAX_START_X ? MAX_START_X :
+				start_x;
+		}
+		void set_start_y(int start_y)
+		{
+			this->start_y =
+				start_y < MIN_START_Y ? MIN_START_Y :
+				start_y > MAX_START_Y ? MAX_START_Y :
+				start_y;
+		}
+		void set_line_width(int line_width)
+		{
+			this->line_width =
+				line_width < MIN_LINE_WIDTH ? MIN_LINE_WIDTH :
+				line_width > MAX_LINE_WIDTH ? MAX_LINE_WIDTH :
+				line_width;
+		}
+		int get_start_x() const
+		{
+			return start_x;
+		}
+		int get_start_y() const
+		{
+			return start_y;
+		}
+		int get_line_width() const
+		{
+			return line_width;
+		}
+		virtual double get_area()const = 0;
+		virtual double get_perimetr()const = 0;
+		virtual void draw()const = 0;
+		virtual void info()const
+		{
+			cout << "Площадь фигуры: " << get_area() << endl;
+			cout << "Периметр фигуры: " << get_perimetr() << endl;
+			draw();
+		}
+	};
+
+	/*class Square :public Shape
+	{
+		double side;
+	public:
+		Square(double side, SHAPE_TAKE_PARAMETRS) :Shape(SHAPE_GIVE_PARAMETRS)
+		{
+			set_side(side);
+		}
+		double get_side()const
+		{
+			return side;
+		}
+		void set_side(double side)
+		{
+			this->side = side;
+		}
+		double get_area()const override
+		{
+			return side * side;
+		}
+		double get_perimetr()const override
+		{
+			return 4 * side;
+		}
+		void draw()const override
+		{
+			for (int i = 0; i < side; i++)
 			{
-				cout << "* ";
+				for (int j = 0; j < side; j++)
+				{
+					cout << "* ";
+				}
+				cout << endl;
 			}
 			cout << endl;
 		}
-		cout << endl;
-	}
-	void info()const override
+		void info()const override
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Длина стороны квадрата: " << get_side() << endl;
+			Shape::info();
+		}
+	};*/
+
+	class Rectangle : public Shape
 	{
-		cout << typeid(*this).name() << endl;
-		cout << "Длина стороны квадрата: " << get_side() << endl;
-		Shape::info();
-	}
-};
+		double width;
+		double height;
+	public:
+		Rectangle(double width, double height, SHAPE_TAKE_PARAMETRS) :Shape(SHAPE_GIVE_PARAMETRS)
+		{
+			set_width(width);
+			set_height(height);
+		}
+		/*double get_width()const
+		{
+			return width;
+		}
+		double get_height()const
+		{
+			return height;
+		}*/
+		void set_width(double width)
+		{
+			this->width = width;
+		}
+		void set_height(double height)
+		{
+			this->height = height;
+		}
+		double get_area()const override
+		{
+			return width * height;
+		}
+		double get_perimetr()const override
+		{
+			return 2 * (width + height);
+		}
+		void draw()const override
+		{
+			HWND hwnd = GetConsoleWindow();  //1) Получаем окно консоли
+			HDC hdc = GetDC(hwnd);			//2) Получаем контекст устройства (DC - Device Context) для окна консоли
+											// DC - это то начем мы будем рисовать
+			//3) Создадим инструменты, которыми мы будем рисовать
+			HPEN hPen = CreatePen(PS_SOLID, line_width, color); //Карандаш (Pen) рисует контур фигуры
+			HBRUSH hBrush = CreateSolidBrush(color);   //Кисть (Brush) рисует заливку фигуры
+			
+			//4)Выберем созданные инструменты:
+			SelectObject(hdc, hPen);
+			SelectObject(hdc, hBrush);
+
+			//5) После того, как все необходимые инструменты созданы и выбраны, можно рисовать
+			::Rectangle(hdc, start_x, start_y, start_x + width, start_y + height);
+
+			//6) hdc, hPen, hBrush занимают ресурсы, а ресурсы нужно освобождать
+			DeleteObject(hBrush);
+			DeleteObject(hPen);
+			
+			ReleaseDC(hwnd, hdc);
+		}
+		void info()const override
+		{
+			cout << typeid(*this).name() << endl;
+			cout << "Стороны: " << width << " x " << height << endl;
+			Shape::info();
+		}
+	};
+
+	class Square : public Rectangle
+	{
+	public:
+		Square (int side, SHAPE_TAKE_PARAMETRS) : Rectangle(side, side, SHAPE_GIVE_PARAMETRS){}
+	};
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	Square square(5, Color::Red);
-	cout << "Длина стороны квадрата: " << square.get_side() << endl;
+	Geometry::Square square(150, 100, 100, 1, Geometry::Color::Red);
+	/*cout << "Длина стороны квадрата: " << square.get_side() << endl;
 	cout << "Площадь квадрата: " << square.get_area() << endl;
 	cout << "Периметр квадрата: " << square.get_perimetr() << endl;
-	square.draw();
+	square.draw();*/
 	cout << "\n-----------------------------------------\n" << endl;
 	square.info();
+	cout << "\n-----------------------------------------\n" << endl;
+	Geometry::Rectangle rectangle(150, 100, 500, 100, 5, Geometry::Color::Orange);
+	rectangle.info();
+	while (true)
+	{
+		square.draw();
+		rectangle.draw();
+	}
 }
